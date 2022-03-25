8845b782118ecc6a5122d27ddc4d9156d5b6c93f
CristinaGarciaRoque <cristinagaroque@gmail.com>
2022-03-25 17:14:54 +0100

    add js event add collapsible

--- src/js/0_collapsables.js
+++ src/js/0_collapsables.js
@@ -6,8 +6,16 @@
 
 /*CONSTANTS GLOBALES*/
 
+const jsDesign = document.querySelector('.js-design');
+const jsStuffed = document.querySelector('.js-stuffed');
+const jsShare = document.querySelector('.js-share');
+const jsArrow1 = document.querySelector('.js-arrow1');
+const jsArrow2 = document.querySelector('.js-arrow2');
+const jsArrow3 = document.querySelector('.js-arrow3');
 
+/*FUNCIONES EVENTOS Y CONDICIONALES*/
 
-
-
+jsArrow1.addEventListener('click',()=>{
+  jsArrow1.classList.toggle('collapsible');
+});
 
