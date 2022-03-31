bca37a3fc42c71f1189d28821a1cf98a97969841
Carmenyo <carmencastroyo@gmail.com>
2022-03-31 21:37:12 +0200

    include data at preview

--- src/js/1_card-colors.js
+++ src/js/1_card-colors.js
@@ -7,30 +7,29 @@ const preview = document.querySelector('.js_preview');
 
 
 function handleClickRadio1(){
-    preview.classList.remove('palette2');
-    preview.classList.remove('palette3');
-    preview.classList.add('palette1');
+  preview.classList.remove('palette2');
+  preview.classList.remove('palette3');
+  preview.classList.add('palette1');
 
 
 }
 
 function handleClickRadio2(){
-    preview.classList.remove('palette1');
-    preview.classList.remove('palette3');
-    preview.classList.add('palette2');
+  preview.classList.remove('palette1');
+  preview.classList.remove('palette3');
+  preview.classList.add('palette2');
 
 }
 
 function handleClickRadio3(){
-    preview.classList.remove('palette1');
-    preview.classList.remove('palette2');
-    preview.classList.add('palette3');
+  preview.classList.remove('palette1');
+  preview.classList.remove('palette2');
+  preview.classList.add('palette3');
 }
 
 radio1.addEventListener('click', handleClickRadio1);
 radio2.addEventListener('click', handleClickRadio2);
 radio3.addEventListener('click', handleClickRadio3);
-
 
 
 
--- src/js/3_include-data.js
+++ src/js/3_include-data.js
@@ -1,2 +1,38 @@
+'use strict';
 ////que se envien los datos del formulario y se previsualicen en la tarjeta
 //
+const forName = document.querySelector('.js-forname');
+const forJob = document.querySelector('.js-forjob');
+const forMail = document.querySelector('.js-formail');
+const forPhone = document.querySelector('.js-forphone');
+const forLkdn = document.querySelector ('.js-forlkdn');
+const forGit = document.querySelector ('.js-forgit');
+
+
+function handleKeyUser () {
+const valueName = forName.value;
+const valueJob = forJob.value;
+const valueMail = forMail.value;
+const valuePhone = foPhone.value;
+const valueLkdn = forLkdn.value;
+const valueGit = forGit.value;
+
+const dataUser = ['object'];
+
+const object = { 
+    name: valueName,
+    job: valueJob,
+    mail: valueMail,
+    phone:valuePhone,
+    lkdn: valueLkdn,
+    git: valueGit,
+};
+
+  js-name
+
+}
+
+dataUser.addEventListener ('keyup', handleKeyUser)
+{
+console.log 
+}
