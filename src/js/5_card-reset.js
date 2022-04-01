8bbf349b5070d48c7fe0b504f45cafa198caf45b
celiarodriguezmo <celiro02@gmail.com>
2022-04-01 20:45:46 +0200

    add functionality at reset button

--- src/js/5_card-reset.js
+++ src/js/5_card-reset.js
@@ -1,1 +1,52 @@
+'use strict';
 //se van a eliminar todos los campos y la imagen de la tarjeta
+const resetBtn = document.querySelector('.js-reset');
+
+const resetName= document.querySelector('.js_reset-name')
+const resetJob= document.querySelector('.js_reset-job');
+const resetMail= document.querySelector('.js_reset-mail');
+const resetPhone= document.querySelector('.js_reset-phone');
+const resetLinkedin= document.querySelector('.js_reset-linkedin');
+const resetGit= document.querySelector('.js_reset-git');
+
+const resetTitle = document.querySelector('.js_reset-title');
+const resetSubtitle = document.querySelector('.js_reset-subtitle');
+const resetTelf = document.querySelector('.js_reset-telf');
+const resetSend = document.querySelector('.js_reset-send');
+const resetLknd = document.querySelector('.js_reset-lknd');
+const resetGitHub = document.querySelector('.js_reset-github');
+
+
+
+
+
+function resetCard() {
+    preview.classList.remove('palette1');
+    preview.classList.remove('palette2');
+    preview.classList.remove('palette3');
+    resetTitle.textContent = 'Nombre Apellido';
+    resetSubtitle.textContent = 'Front-end developer';
+    resetTelf.href = "";
+    resetSend.href = "";
+    resetLknd.href = "";
+    resetGitHub.href = "";
+}
+
+function resetForm (){
+
+    resetName.value="";
+    resetJob.value="";
+    resetMail.value="";
+    resetPhone.value="";
+    resetLinkedin.value="";
+    resetGit.value="";
+    
+}
+
+function handleResetForm (event){
+    event.preventDefault();
+    resetForm ();
+    resetCard ();
+}
+
+resetBtn.addEventListener('click', handleResetForm);
